d69546d16ec69ed5f845f27399794893eb18bac9
Daan Thijs <daan.thijs1@student.kuleuven.be>
2018-08-09 13:53:40 +0200

    Added login screen.

--- App.js
+++ App.js
@@ -1,17 +1,65 @@
 //using react navigation!
-import React from 'react';
-import { Button, View, Text } from 'react-native';
+import React , { Component } from 'react';
+import { Button, View, Text, Alert, TextInput, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+});
 
 class LoginScreen extends React.Component {
+
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        username: '',
+        password: '',
+      };
+    }
+
+    onLogin() {
+    const { username, password } = this.state;
+
+    Alert.alert('Credentials', `${username} + ${password}`);
+    this.props.navigation.navigate('Home');
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Login screen</Text>
+
+      <View style={styles.container}>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+          placeholder={'Username'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+
         <Button
-          title="Login"
-          onPress={() => this.props.navigation.navigate('Home')}
+          title={'Login'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
         />
       </View>
     );
